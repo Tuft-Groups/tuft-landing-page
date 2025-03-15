a2c66c8ed59b80087d247730ae74d5421a566da7
Neerajsameer <neerajsameerallu00@gmail.com>
2025-03-15 15:57:13 +0530

    deployy

---
+++ src/pages/group/[id].tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+
+export async function getServerSideProps({ params }: { params: { id: string } }) {
+  const room = await axios.get(`https://tuft-core-wq7gvvjxpa-el.a.run.app/rooms/${params.id}/preview`);
+  console.log({ room: room.data.data });
+  return {
+    props: { room: room.data.data },
+  };
+}
+
+export default function RoomPreview({ room }: { room: any }) {
+  if (!room) return <div>Room not found</div>;
+
+  return (
+    <div className="flex flex-col h-screen grow  max-w-[400px] m-auto ">
+      <div className="flex flex-col gap-4 h-full p-6">
+        <div className="h-full overflow-y-scroll">
+          <div className="relative mb-10">
+            {room.cover_image_url ? (
+              <img src={room.cover_image_url} alt="Room cover" className="w-full h-48 object-cover rounded-lg" />
+            ) : (
+              <div className="w-full h-16"></div>
+            )}
+            <div className="absolute -bottom-6 left-0">
+              <img
+                src={room.avatar || ""}
+                alt="Room logo"
+                className="size-20 rounded-full border-4 border-background"
+              />
+            </div>
+          </div>
+
+          <div>
+            <h1 className="text-2xl font-bold">{room.name}</h1>
+            <ReactMarkdown>{room.description || room.short_description}</ReactMarkdown>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Button
+            className="w-full"
+            onClick={() => {
+              window.location.href = "https://app.tuft.in/group/" + room.id;
+            }}
+          >
+            Join Room
+          </Button>
+          <div className="flex items-center justify-center gap-2">
+            <span className="text-muted-foreground text-xs">Powered by</span>
+            <img src="/assets/images/long_logo.webp" alt="Tuft" width={30} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
