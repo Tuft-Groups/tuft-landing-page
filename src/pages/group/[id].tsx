c3b6b3f1618848e9d65b3548c724ed0bbfb5524a
Neerajsameer <neerajsameerallu00@gmail.com>
2025-03-15 16:19:38 +0530

    deploy

--- src/pages/group/[id].tsx
+++ src/pages/group/[id].tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 import axios from "axios";
 import ReactMarkdown from "react-markdown";
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 export async function getServerSideProps({ params }: { params: { id: string } }) {
   const room = await axios.get(`https://tuft-core-wq7gvvjxpa-el.a.run.app/rooms/${params.id}/preview`);
-  console.log({ room: room.data.data });
+
   return {
     props: { room: room.data.data },
   };
